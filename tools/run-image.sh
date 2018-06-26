99d3eb01b10b0052a192b39dd7ecb7b106817702
Erik Pearson <eapearson@lbl.gov>
2018-06-26 15:51:47 -0700

    put local narrative back in

--- tools/run-image.sh
+++ tools/run-image.sh
@@ -4,14 +4,16 @@ function usage() {
 
 environment=$1
 
-if [ -z "$environment" ]; then 
+if [ -z "$environment" ]
+then 
     echo "ERROR: argument 1, 'environment', not provided"
     usage
     exit 1
 fi
 
 network=$2
-if [ -z "$network" ]; then
+if [ -z "$network" ]
+then
     echo "ERROR: argument 2, 'network', not provided"
     usage
     exit 1
@@ -21,10 +23,19 @@ root=$(git rev-parse --show-toplevel)
 config_mount="${root}/conf"
 image="kbase/kbase-ui-proxy:dev"
 
-if [ ! -e "${root}/conf/${environment}.env" ]; then
+if [ ! -e "${root}/conf/${environment}.env" ]
+then
     echo "ERROR: environment (arg 1) does not resolve to a config file in ${root}/conf/${environment}.env"
     usage
     exit 1
+fi
+
+# A better solution coming soon...
+options=""
+dq = '"'
+if [ -n "${local_narrative}" ]
+then
+    options=' -e "''local_narrative=true''"'
 fi
 
 
@@ -32,11 +43,17 @@ echo "CONFIG MOUNT: ${config_mount}"
 echo "ENVIRONMENT : ${environment}"
 
 echo "Running proxy image ${image}"
+echo "with options ${options}"
 echo ":)"
 
-docker run \
+cmd="docker run \
   -p 80:80 -p 443:443 --dns=8.8.8.8 --rm \
   --env-file=${config_mount}/${environment}.env \
+  ${options} \
   --network=${network} \
   --name=proxy \
-  ${image} 
+  ${image}"
+
+echo "running: ${cmd}"
+  
+eval $cmd
